3e60a2ebeac8efc9e1f8cfb408e0cb4d5dc99ffb
malikkamranabid <malikkamranabid@gmail.com>
2023-08-08 19:35:23 +0500

    changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/gfg');
+mongoose.connect('mongodb://localhost:27017/newgfg');
 const PORT = process.env.PORT || 5001;
 
 
